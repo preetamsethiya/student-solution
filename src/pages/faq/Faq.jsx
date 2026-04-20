700fed57bcc967701ae9a1d9556b4046711300b0
preetam kumar <preetamsethiya6@gmail.com>
2026-04-20 15:55:02 +0530

    parsent calculator add

--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -3,6 +3,7 @@ import Page_Container from "../../components/page_container/Page_Container";
 import Button from "../../components/button/Button";
 import "../../App.css";
 import { results } from "../../data_store/data";
+import Input from "../../components/input/Input";
 
 export default function Faq() {
   const FAQs = [
@@ -20,26 +21,49 @@ export default function Faq() {
     },
   ];
   const [showAns, setShowAns] = useState(null);
-
+  const [total, setTotal] = useState(0);
+  const [obtained, setObtained] = useState(0);
+  let patsent = (parseInt(obtained) * 100) / parseInt(total);
   return (
     <div>
       <Page_Container>
         <div>
           <div>
-            <h3 className="pl-3 font-semibold  mb-2"> Results </h3>
+            <h3 className="pl-3 font-semibold  mb-2 text-2xl"> Results </h3>
+            <div className="flex items-center gap-2 pl-3 mb-2 ">
+              <input
+                className="ring-1 p-1 w-[140px] ring-blue-500 outline-none "
+                type="number"
+                placeholder="total marks"
+                onChange={(e) => {
+                  setTotal(e.target.value);
+                }}
+              />
+              <input
+                className="ring-1 p-1 w-[140px] ring-blue-500 outline-none "
+                type="number"
+                placeholder="obtained marks "
+                onChange={(e) => {
+                  setObtained(e.target.value);
+                }}
+              />
+              <div>
+                {`${(total && obtained) < 1 ? "00" : patsent.toFixed(2)}`}%
+              </div>
+            </div>
           </div>
 
           <div className="bg-gray-300 p-2 rounded-sm">
-            {results.map((result) => {
+            {results.map((result, i) => {
               return (
-                <div>
+                <div key={i}>
                   <div className="bg-blue-400 font-semibold px-4 ">
                     {result.course}
                   </div>
                   <div className="grid grid-cols-[repeat(auto-fill,minmax(90px,1fr))] gap-[6px] bg-pink-300 p-[6px] text-center ">
-                    {result.programmes.map((programme) => {
+                    {result.programmes.reverse().map((programme, i) => {
                       return (
-                        <a href={programme.url} target="_blank">
+                        <a key={i} href={programme.url} target="_blank">
                           <div className=" flex items-center justify-center flex-col">
                             <div
                               className={`h-[70px] w-[70px] rounded-[85px] flex items-center justify-center ${programme?.color}
